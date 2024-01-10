158178419b7a97e62052ee86a9abda3951e86252
reinisciemins <144588742+reinisciemins@users.noreply.github.com>
2024-01-10 19:04:13 +0200

    Update project.py

--- project.py
+++ project.py
@@ -1,1 +1,18 @@
+import os
 
+def main( ):
+    # notira konsoli
+    os.system( "cls" )
+
+    print( "C/C++ klases rekonstruktors - Reinis Ciemiņš 231RDB191" )
+    print( "------------------------------------------------------" )
+
+    file_name = str( input( "Ievadiet faila nosaukumu: " ) )
+    file_name += ".hpp"
+
+    class_name = str( input( "Ievadiet klases nosaukumu: " ) )
+
+    return
+
+if __name__ == "__main__":
+    main( )
