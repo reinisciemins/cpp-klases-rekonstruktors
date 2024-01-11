1e9864b5b5d63df0d105c1f81be9c0ca985a0d68
reinisciemins <144588742+reinisciemins@users.noreply.github.com>
2024-01-11 15:08:20 +0200

    Update project.py

--- project.py
+++ project.py
@@ -4,15 +4,31 @@ def main( ):
     # notira konsoli
     os.system( "cls" )
 
+    # sakuma teksts
     print( "C/C++ klases rekonstruktors - Reinis Ciemiņš 231RDB191" )
     print( "------------------------------------------------------" )
 
-    file_name = str( input( "Ievadiet faila nosaukumu: " ) )
+    # faila nosaukuma iegusana
+    file_name = str( input( "-> Ievadiet faila nosaukumu: " ) )
     file_name += ".hpp"
 
-    class_name = str( input( "Ievadiet klases nosaukumu: " ) )
+    # tiek izveidots fails
+    file = open( file_name, "w+" )
 
-    return
+    # pievieno klases definiciju
+    class_name = str( input( "-> Ievadiet klases nosaukumu: " ) )
+    file.write( "#pragma once\n\nclass " + class_name + " {\npublic:" )
+
+    # cik mainigos nepieciesams pievienot
+    member_count = int( input( "-> Ievadiet klases mainīgo skaitu: " ) )
+
+    # aizver izveidoto failu
+    file.close( )
+
+    # nosleguma teksts
+    print( "------------------------------------------------------" )
+    print( file_name + " izveidota klase " + class_name + "!" )
+    print( "------------------------------------------------------" )
 
 if __name__ == "__main__":
     main( )
